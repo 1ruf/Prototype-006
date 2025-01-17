f853933cc8f6a8bbeb2064f3310bc79680e1c0e5
1ruf <gyeol0411@naver.com>
2025-01-17 15:34:45 +0900

    .update MenuScene

---
+++ Assets/2.Scripts/MenuScene/Camera/CameraMovement.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _camLooker;
+
+
+}
