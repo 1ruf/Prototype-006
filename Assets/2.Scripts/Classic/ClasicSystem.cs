67bfcb2f459aee25e6bbfa985261ba40e7d1703b
1ruf <gyeol0411@naver.com>
2025-01-22 01:08:07 +0900

    .add TurnSystem | .add GunOutlineShader | .add GunSelectSystem | .update ClassicScene
    develop ver 1.2.01

--- Assets/2.Scripts/Classic/ClasicSystem.cs
+++ Assets/2.Scripts/Classic/ClasicSystem.cs
@@ -1,20 +1,25 @@
 using DG.Tweening;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using UnityEngine.UIElements.Experimental;
 
 public class ClasicSystem : MonoBehaviour
 {
     [SerializeField] private GameObject _blocker;
     [SerializeField] private Animator _cutsceneAnimator;
     [SerializeField] private GameObject _revolver;
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI _orderTMP;
     [Header("Setting")]
     [SerializeField] private float _introFadeTime;
 
     private int _currentTurn; //0 = plr, 1 = bot
     private void OnEnable()
     {
+        _orderTMP.text = "";
         StartCoroutine(Fadeout(1f));
     }
 
@@ -41,12 +46,58 @@ public class ClasicSystem : MonoBehaviour
             _cutsceneAnimator.Play("Starter_BOT");
         }
         StartCoroutine(SetAnimator(_cutsceneAnimator, false, 4.5f));
+        CheckTurn(4.5f);
     }
+
+    private void CheckTurn(float time = 0)
+    {
+        StartCoroutine(CheckTurnCoroutine(time));
+    }
+    private IEnumerator CheckTurnCoroutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+        if (_currentTurn == 0)
+        {
+            PlayerTurn();
+        }
+        else
+        {
+            EnemyTurn();
+        }
+    }
+
+    public void PlayerTurn()
+    {
+        _orderTMP.text = "Grab the gun";
+        StartCoroutine(SetGunClick(true));
+
+
+
+        _currentTurn = 1;//1 == enemy
+    }
+
+    public void EnemyTurn()
+    {
+        print("상대 턴 입니다.");
+
+
+
+        _currentTurn = 0;//0 == player
+    }
+
+
+
 
     private IEnumerator SetAnimator(Animator animator,bool value, float time = 0)
     {
         yield return new WaitForSeconds(time);
         animator.enabled = value;
+    }
+
+    private IEnumerator SetGunClick(bool value, float time = 0)
+    {
+        yield return new WaitForSeconds(time);
+        _revolver.GetComponent<Gun>().SetGunClick(value);
     }
 }
 
---
+++ Assets/2.Scripts/Classic/Gun.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private string _outlineLayerName = "Outline";
+    [SerializeField] private string _gunLayerName = "Gun";
+
+    private bool _canClick;
+
+    public void SetGunClick(bool value)
+    {
+        _canClick = value;
+    }
+
+    private void OnMouseEnter()
+    {
+        SetLayer(gameObject, LayerMask.NameToLayer(_outlineLayerName));
+    }
+    private void OnMouseExit()
+    {
+        SetLayer(gameObject, LayerMask.NameToLayer(_gunLayerName));
+    }
+
+    private void SetLayer(GameObject target,int targetLayer)
+    {
+        if (_canClick == false) return;
+        ChangeLayerRecursively(target, targetLayer);
+    }
+
+    //----------------------by.ChatGPT----------------------
+    /// <summary>
+    /// 오브젝트와 자식들의 레이어를 재귀적으로 변경하는 함수
+    /// </summary>
+    /// <param name="obj">변경할 게임 오브젝트</param>
+    /// <param name="layer">변경할 레이어 ID</param>
+    private void ChangeLayerRecursively(GameObject obj, int layer)
+    {
+        if (obj == null) return;
+
+        // 현재 오브젝트의 레이어를 변경
+        obj.layer = layer;
+
+        // 자식 오브젝트가 있다면 재귀적으로 레이어 변경
+        foreach (Transform child in obj.transform)
+        {
+            ChangeLayerRecursively(child.gameObject, layer);
+        }
+    }
+}
